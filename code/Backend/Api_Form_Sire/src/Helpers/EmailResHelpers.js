e8788fbd504e79d4137cf095f9713598fe53a73c
Lucas M. Pedrozo P <lpedrozo@utb.edu.co>
2024-05-30 17:15:46 -0500

    Delete code/Backend/Api_Form_Sire directory

--- code/Backend/Api_Form_Sire/src/Helpers/EmailResHelpers.js
+++
@@ -1,8 +0,0 @@
-export function HtmlModificado(htmlContent, valores) {
-    let contenidoModificado = htmlContent;
-    for (const marcador in valores) {
-        const expresionRegular = new RegExp(`\\[${marcador}\\]`, 'g');
-        contenidoModificado = contenidoModificado.replace(expresionRegular, valores[marcador]);
-    }
-    return contenidoModificado;
-}
