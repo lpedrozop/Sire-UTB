46eab8578568de9d6eb8fc5ae23d21f058a62481
Lucas M. Pedrozo P <lpedrozo@utb.edu.co>
2024-05-30 16:53:25 -0500

    Delete code/Backend/Api_Admi_Sire - copia directory

--- code/Backend/Api_Admi_Sire - copia/src/Helpers/ValidateHour.js
+++
@@ -1,6 +0,0 @@
-export const validateTimeFormat = (timeString) => {
-    const timeFormat = /^([0-1][0-9]|2[0-3]):[0-5][0-9]-([0-1][0-9]|2[0-3]):[0-5][0-9]$/;
-    return timeFormat.test(timeString);
-}
-
-
--- code/Backend/Api_Admi_Sire - copia/src/index.js
+++
@@ -1,6 +0,0 @@
-import {PORT} from "./Config/Config.js";
-import app from "./app.js"
-
-app.listen(PORT, () => {
-    console.log('servidor listo', PORT);
-});
