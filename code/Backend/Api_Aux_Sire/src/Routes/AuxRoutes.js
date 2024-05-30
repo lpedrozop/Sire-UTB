7a221bd732c1bd4d3a05754d70d95b00495eb20b
Lucas M. Pedrozo P <lpedrozo@utb.edu.co>
2024-05-30 17:08:30 -0500

    Delete code/Backend/Api_Aux_Sire/src directory

--- code/Backend/Api_Aux_Sire/src/Routes/AuxRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {CrearReporte, ObtenerEspaciosAux} from "../Controllers/AuxController.js";
-
-const router = express.Router();
-
-router.get('/espaciosaux', ObtenerEspaciosAux)//Devuelve todos los espacios donde hay una reserva o una clase que estén asignados al auxiliar
-router.post('/reporte', CrearReporte)//Crea un reporte de una reserva o una clase regular
-
-export default router;
